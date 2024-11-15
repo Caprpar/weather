2c82fb8a511f82abfbc3f26304f49c7d1db010d6
Korscao <felix.korshavn@iths.se>
2024-11-15 14:37:54 +0100

    loopande av stationerna in i menyn

--- script.js
+++ script.js
@@ -2,17 +2,22 @@
 //loop to get the stations and adding a div 'li' for each station
 
 for (let i = 0; i < stations.length; i++){
-    let station = document.createElement('li')
-    station.id = i
-    station.addEventListener('click', updateStation)
+    let li = document.querySelector('li')
+
+    let nameOfStation = document.createTextNode('weatherData.name')
+
+    li.appendChild(nameOfStation)
+
+    li.id = i
+    li.addEventListener('click', updateStation)
+
 
 }
 
+//function to get the new station. get the div ID and
+
 function updateStation() {
     document.querySelector('#weather-data').textContent = weatherData.name
-
-
-
 }
 
 console.log(updateStation)
