6ac94f0f022e076387820dccda4896f9cd26a357
GlassesMan-031 <karelindqvist@gmail.com>
2024-11-15 14:56:33 +0100

    Built pseudocode for changing overcast picture

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /**pseudocode for inputting images depending on temperature*/
 displayWeatherImagePseudo(weatherData) {
-  function changePicture(weatherData) {
+  function changePictureTemperature(weatherData) {
     const temperatureValue = getWeatherValue();
 
     let imgElement = document.getElementById('myImage');
+    //could be a stylesheet link aswell, only temporary
 
     if (temperatureValue < 0) {
       imgElement.src = 'negativeDegrees.jpg';
@@ -12,5 +13,25 @@ displayWeatherImagePseudo(weatherData) {
     } else {
       imgElement.src = 'positiveDegrees.jpg';
     }
+  }
+
+  /**pseudocode for inputting images depending on overcast*/
+  function changePictureOvercast(weatherData) {
+    const overcastValue = getWeatherValue();
+
+    let imgElement = document.getElementById('myImage');
+    //could be a stylesheet link aswell, only temporary
+    if (overcastValue < 50) {
+      imgElement.src = 'clearSkies.jpg';
+    } else if (overcastValue >= 50 && overcastValue < 75) {
+      imgElement.src = 'sligthlyOvercast.jpg';
+    } else (overcastValue >= 75){
+      imgElement.src = 'overcast.jpg'
+    }
+  }
+
+  /**pseudocode for inputting images depending on downfall*/
+  function downfallValue(weatherData) {
+    const downfallValue = getWeatherValue();
   }
 }
