089a5d6655ac454f28d9792f83815b322895fd40
Korscao <felix.korshavn@iths.se>
2024-11-15 16:12:40 +0100

    For loop som ska loopa in alla stationer till menyn

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
  * }
  *
  */
-let stations = []
+let stations = [];
 
 let weatherData = {
   id: 0,
@@ -18,13 +18,13 @@ let weatherData = {
   weatherType: '' //type of weather, in string
 }
 
-function getStations(stationsOld) {
+async function getStations(stationsOld) {
   let stationsNew = [] // create new temporary array to house the new stations
-  fetch("https://opendata-download-metobs.smhi.se/api/version/latest/parameter/1/station-set/all/period/latest-hour/data.json").then((response) => {
+  stationsOld = await fetch("https://opendata-download-metobs.smhi.se/api/version/latest/parameter/1/station-set/all/period/latest-hour/data.json").then((response) => {
     // console.log(response)
     return response.json() // return the fetch response in json
   }).then((result) => {
-    // console.log(result.station[0])
+     //console.log(result.station[0])
     result.station.forEach(station => { // for every station in station(from response)
       // push a new object into new stations, which has id and name
       stationsNew.push({
@@ -33,11 +33,37 @@ function getStations(stationsOld) {
       })
     });
     // replaces old stations with new stations content
-    stationsOld = stationsNew
-    // console.log(stationsOld.length)
+    // console.log(stationsNew)
+    return stationsNew
   }).catch((err) => {
     console.log(err) // logs an error if anything would go wrong
   })
+
+}
+getStations(stations)
+
+console.log(stations)
+
+function getStationsMenu(stations) {
+    //menu to loop the stations
+    console.log("hej")
+    for (let i = 0; i < stations.length; i++){
+
+        console.log("hejhej")
+
+        let ol = document.querySelector('ol') //getting the orderd-list
+        let nameOfStation = document.createTextNode(stations[i].name) //creating a node with the name och stations name
+        ol.appendchild(nameOfStation) //adding a child with the name of station for each "i"
+        nameOfStation.addEventListener('click', updateStation) //adding a click event to the list
+    }
+
+
 }
 
-getStations(stations)
+getStationsMenu(stations)
+
+
+
+// function updateStation() {
+//     document.querySelector('li') = weatherData["name"]
+// }
